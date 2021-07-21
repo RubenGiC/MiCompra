da0b6534c1f9744d3794224f67eb3d8f318b6aa3
RubenGiC <rubengirela@gmail.com>
2021-07-21 14:25:02 +0200

    Add function tu take picture and formulary

---
+++ app/src/main/java/org/rubengic/micompra/AddItem.java
@@ -0,0 +1,141 @@
+package org.rubengic.micompra;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class AddItem extends AppCompatActivity {
+
+    //directory app
+    private String APP_DIRECTORY = "ImagesMiCompra";
+    //directory media
+    private String MEDIA_DIRECTORY = APP_DIRECTORY + "media";
+    //directory where the image save temporality
+    private String TEMPORAL_PICTURE_NAME = "temp.jpg";
+
+    //code image (code respond)
+    private final int PHOTO_CODE = 100;
+    private final int SELECT_PICTURE = 200;
+
+    private ImageView img_v;
+    private Bitmap imageBitmap;
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    //take the picture
+    private void dispatchTakePictureIntent() {
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), MEDIA_DIRECTORY);
+        //create directories
+        file.mkdirs();
+        //create the path
+        String path = getExternalFilesDir(Environment.DIRECTORY_PICTURES) + File.separator + MEDIA_DIRECTORY + File.separator + TEMPORAL_PICTURE_NAME;
+
+        File newfile = new File(path);
+
+        //create intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //if isn't null the take picture
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //capture and save image
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(newfile));
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //create image and show
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            imageBitmap = (Bitmap) extras.get("data");
+            img_v.setImageBitmap(imageBitmap);
+        }
+    }
+    //path of the picture
+    String currentPhotoPath;
+    //save the image
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+
+        //charge the imagen view and button
+        img_v = (ImageView) findViewById(R.id.i_image);
+        Button b = (Button) findViewById(R.id.b_add_image);
+
+        //action click of the button
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //take a picture
+                dispatchTakePictureIntent();
+                //options
+                //final CharSequence[] options = {"hacer foto", "Elegir foto de galeria", "Cancelar"};
+                //Dialog for choose options
+               /* final AlertDialog.Builder builder = new AlertDialog.Builder(AddItem.this);
+                //add title
+                builder.setTitle("Elija una opcion");
+                //add the options
+                builder.setItems(options, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if(options[which] == "hacer foto"){
+                            openCamera();
+                        }else if(options[which] == "Elegir foto de galeria"){
+                            //create the intent by access to galery
+                            Intent intento = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            //acept all types of images
+                            intento.setType("image/*");
+                            //choose the app to select the picture
+                            startActivityForResult(intento.createChooser(intento, "Selecciona la app de imagenes"), SELECT_PICTURE);
+
+                        }else{
+                            dialog.dismiss();
+                        }
+                    }
+                });
+                builder.show();*/
+            }
+        });
+    }
+}
---
+++ app/src/main/java/org/rubengic/micompra/MainActivity.java
@@ -0,0 +1,84 @@
+package org.rubengic.micompra;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import org.rubengic.micompra.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityMainBinding binding;
+
+    private FloatingActionButton fab_add;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        fab_add = (FloatingActionButton) findViewById(R.id.fab_add);
+        fab_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Añadiendo nuevo producto", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+                //add action add new item
+                Intent i= new Intent(getApplicationContext(), AddItem.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
